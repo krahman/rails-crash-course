296cf1bd060b80d49a094b6c5795e1444375e7fc
Khalilur Rahman <khal.rahman@gmail.com>
2015-03-01 13:25:47 +0800

    Added author field in post table

--- rails-fundamentals/code/blog/db/schema.rb
+++ rails-fundamentals/code/blog/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228170016) do
+ActiveRecord::Schema.define(version: 20150301045324) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
 end
