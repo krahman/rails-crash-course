4b9d1e2a0bca3e22d9e9d85e5af2f29f0c41ea2f
Khalilur Rahman <khal.rahman@gmail.com>
2015-03-03 01:59:37 +0800

    Added comment controller

---
+++ rails-fundamentals/code/blog/app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+
+        if @post.comments.create(comment_params)
+            redirect_to @post, notice: 'Comment was successfully created,'
+        else
+            redirect_to @post, alert: 'Error creating comment.'
+        end
+    end
+
+    def comment_params
+        params.require(:comment).permit(:author, :body)
+    end
+end
